a71ab9aa67e4bf5479bec8bded8d409dde442680
Arun <expertaruncorp@gmail.com>
2018-05-19 13:49:51 +0545

    Added sqlite-jdbc lib and db

--- src/hospital_management_system/Hospital_Management_System.java
+++ src/hospital_management_system/Hospital_Management_System.java
@@ -4,19 +4,38 @@
  * and open the template in the editor.
  */
 package hospital_management_system;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author aryan
  */
 public class Hospital_Management_System {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.print("Hello Hospital");
+ public static void connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:database.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+ public static void main(String[] args) {
+        connect();
     }
     
 }
